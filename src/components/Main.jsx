d7f786dc949a982c9ec0f5504abbe458b931828d
Gala Chernikova <gala.chernikova94@gmail.com>
2023-02-23 16:22:02 +0300

    Update main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,42 @@
 import { NavLink } from "react-router-dom";
 import { Fade } from "react-awesome-reveal";
+import { FiCheckSquare, FiUserPlus, FiSmile } from "react-icons/fi";
 
 export default function Main() {
   return (
     <div className="flex flex-col bg-main min-h-screen bg-cover bg-no-repeat bg-center">
-      <h1 className="font-title text-9xl text-red-50 mx-auto drop-shadow-xl mb-24 mt-auto max-w-5xl w-full text-center lg:text-7xl lg:mb-16 md:text-5xl md:max-w-min sm:text-3xl">
+      <h1 className="font-title text-9xl text-red-50 mx-auto drop-shadow-xl mb-24 mt-auto max-w-5xl w-full text-center lg:text-7xl lg:mb-16 md:text-5xl md:max-w-min sm:text-3xl sm:mb-10">
         <Fade cascade damping={0.1}>
           My TO-DO app
         </Fade>
       </h1>
 
-      <div className="flex justify-between font-main text-red-50 drop-shadow-xl text-3xl max-w-4xl w-full mx-auto text-center mb-16 lg:text-xl lg:max-w-xl lg:mb-12 md:flex-col md:max-w-min md:text-start">
+      <div className="flex justify-between font-main text-red-50 drop-shadow-xl text-3xl max-w-4xl w-full mx-auto text-start mb-16  lg:max-w-lg lg:mb-12 lg:flex-col md:max-w-min md:text-start sm:text-base sm:w-72 sm:mb-8">
         <Fade cascade>
-          <p className="w-56 lg:w-44 md:mb-10">Make your own to-do list</p>
-          <p className="w-56 lg:w-44 md:mb-10">Add friends and share your tasks</p>
-          <p className="w-56 lg:w-36 md:mb-10">Share motivation</p>
+          <div className="flex lg:items-baseline">
+            {FiCheckSquare()}
+            <p className="w-56 ml-3 lg:w-96 lg:mb-10 sm:mb-5"> Make your own to-do list</p>
+          </div>
+          <div className="flex lg:items-baseline">
+            {FiUserPlus()}
+            <p className="w-56 ml-3 lg:w-96 lg:mb-10 sm:mb-5">
+              Add friends and share your tasks
+            </p>
+          </div>
+          <div className="flex lg:items-baseline">
+            {FiSmile()}
+            <p className="w-56 ml-3 lg:w-96 lg:mb-10 sm:mb-5">Share motivation</p>
+          </div>
         </Fade>
       </div>
 
       <NavLink
         to="/general"
-        className="text-4xl font-main text-red-50 border-red-50 border-2 rounded-3xl drop-shadow-xl py-4 px-10 mb-auto mx-auto hover:text-stone-600 hover:bg-red-50 lg:text-2xl"
+        className="text-4xl font-main text-red-50 border-red-50 border-2 rounded-3xl drop-shadow-xl py-4 px-10 mb-auto mx-auto hover:text-stone-600 hover:bg-red-50 lg:text-2xl sm:text-base"
       >
         <Fade cascade damping={0.1}>
-            START &#8594;
+          START &#8594;
         </Fade>
-        
       </NavLink>
     </div>
   );
